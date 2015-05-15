3035d0f8c965cd768443e69126b2aebeb95bc025
MoritzGoeckel <moritz.goeckel@gmail.com>
2015-05-15 11:12:12 +0200

    Now runnable! :)

--- Terrain.js
+++ Terrain.js
@@ -6,16 +6,18 @@ function Terrain(position, size, resolution, heightmap)
     var material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
 
     //Rotate it
-    geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+    //geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
 
     var vertices = geometry.attributes.position.array;
 		for ( var i = 0; (i * 3) < vertices.length && i < heightmap.length; i++) {
-      vertices[i * 3 + 2] = heightmap[i];
+      vertices[(i * 3) + 2] = heightmap[i];
 		}
 
     this.mesh = new THREE.Mesh( geometry, material);
 
+    this.mesh.receiveShadow = true;
     this.mesh.castShadow = true;
+
     this.mesh.position.set(this.position.x, this.position.y, this.position.z);
 
     this.addToScene = function(scene)
