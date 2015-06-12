ac492bf01fac061aa2d0bf6ac8281fa2b739b8a2
xenon <moritz.goeckel@gmail.com>
2015-06-12 16:18:19 +0200

    Added color :)

--- Terrain.js
+++ Terrain.js
@@ -3,16 +3,21 @@ function Terrain(position, size, heightmap)
     this.position = position;
 
     var geometry = new THREE.PlaneBufferGeometry(size, size, size, size);
-    var material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+    var material = new THREE.MeshLambertMaterial( { color: 0xd0d0d0 } );
 
     //Rotate it
     //geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
 
     var vertices = geometry.attributes.position.array;
 		for ( var i = 0; (i * 3) < vertices.length && i < heightmap.length; i++) {
-      if(isNaN(heightmap[i]) == false)
+      if(isNaN(heightmap[i]) === false)
         vertices[(i * 3) + 2] = heightmap[i];
 		}
+
+    /*this.mesh = THREE.SceneUtils.createMultiMaterialObject( geometry, [
+      new THREE.MeshLambertMaterial( { color: 0xd0d0d0 } ),
+      new THREE.MeshBasicMaterial( { color: 0xd0d0d0, wireframe: true} )
+    ]);*/
 
     this.mesh = new THREE.Mesh( geometry, material);
 
